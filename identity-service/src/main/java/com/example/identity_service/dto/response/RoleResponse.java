17cf3c461916a5efc55b1151b4f439fdc8bf003c
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-08-19 12:24:19 +0700

    identity service

---
+++ identity-service/src/main/java/com/example/identity_service/dto/response/RoleResponse.java
@@ -0,0 +1,18 @@
+package com.example.identity_service.dto.response;
+
+
+import java.util.Set;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RoleResponse {
+    String name;
+    String description;
+    Set<PermissionResponse> permissions;
+}
