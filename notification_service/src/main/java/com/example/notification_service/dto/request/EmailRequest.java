f6339db98f6ef92aef39ed99c2bb724fab990c37
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-08-23 10:51:41 +0700

    add send email

---
+++ notification_service/src/main/java/com/example/notification_service/dto/request/EmailRequest.java
@@ -0,0 +1,19 @@
+package com.example.notification_service.dto.request;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class EmailRequest {
+    Sender sender;
+    List<Recipient> to;
+    String subject;
+    String htmlContent;
+}
