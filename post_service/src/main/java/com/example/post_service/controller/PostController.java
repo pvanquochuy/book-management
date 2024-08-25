91168d8b109ecb5dd46ffc63376af1c9be457809
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-08-25 13:37:18 +0700

    pagination

--- post_service/src/main/java/com/example/post_service/controller/PostController.java
+++ post_service/src/main/java/com/example/post_service/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.example.post_service.controller;
 
 import com.example.post_service.dto.ApiResponse;
+import com.example.post_service.dto.PageResponse;
 import com.example.post_service.dto.request.PostRequest;
 import com.example.post_service.dto.response.PostResponse;
 import com.example.post_service.service.PostService;
@@ -8,10 +9,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,9 +28,12 @@ public class PostController {
     }
 
     @GetMapping("/my-posts")
-    ApiResponse<List<PostResponse>> myPosts(){
-        return ApiResponse.<List<PostResponse>>builder()
-                .result(postService.getMyPosts())
+    ApiResponse<PageResponse<PostResponse>> myPosts(
+            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(value = "size", required = false, defaultValue = "10") int size
+    ){
+        return ApiResponse.<PageResponse<PostResponse>>builder()
+                .result(postService.getMyPosts(page, size))
                 .build();
     }
 }
